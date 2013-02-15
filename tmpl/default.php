c2ab830baab6cd912274b026241d6929126376ea
Matt Thomas <matt@betweenbrain.com>
2013-02-14 22:38:13 -0500

    add collections div

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,54 +12,56 @@
  */
 
 if ($items) : ?>
-<div class="sd-strip-controls-container">
-	<div class="sd-strip-controls">
-		<img class="control sd-back hvr" src="http://media.guggenheim.org/rotator/flickr/carousel_left.jpg" alt="Back" /><img class="control sd-forward hvr" src="http://media.guggenheim.org/rotator/flickr/carousel_right.jpg" alt="Forward" />
-	</div>
-	<div class="sd-strip-container">
-		<ul class="sd-strip">
-			<?php foreach ($items as $item) : ?>
-			<li>
+<div class="collections<?php echo $moduleclass_sfx ?>">
+	<div class="sd-strip-controls-container">
+		<div class="sd-strip-controls">
+			<img class="control sd-back hvr" src="http://media.guggenheim.org/rotator/flickr/carousel_left.jpg" alt="Back" /><img class="control sd-forward hvr" src="http://media.guggenheim.org/rotator/flickr/carousel_right.jpg" alt="Forward" />
+		</div>
+		<div class="sd-strip-container">
+			<ul class="sd-strip">
+				<?php foreach ($items as $item) : ?>
+				<li>
 
-				<?php if (isset($item['link'])) : ?>
+					<?php if (isset($item['link'])) : ?>
 			<a href="<?php echo $item['link'] ?>">
 			<?php endif ?>
 
-				<?php if (isset($item['media'])) : ?>
-				<img src="<?php echo $item['media'] ?>" />
-				<?php endif ?>
-
-				<?php if (isset($item['link'])) : ?>
-			</a>
-			<?php endif ?>
-
-				<div class="content">
-					<?php echo $item['title'] ?>
-
-					<?php if (isset($item['name'])) : ?>
-					<?php if (isset($item['bioUrl'])) : ?>
-						<a href="<?php echo $item['bioUrl'] ?>">
-					<? endif ?>
-
-					<?php echo $item['name'] ?>
-
-					<?php if (isset($item['bioUrl'])) : ?>
-					</a>
-					<? endif ?>
-					<?php endif ?>
-
-					<?php if (isset($item['date'])) : ?>
-					<p class="date"><?php echo $item['date'] ?></p>
+					<?php if (isset($item['media'])) : ?>
+					<img src="<?php echo $item['media'] ?>" />
 					<?php endif ?>
 
 					<?php if (isset($item['link'])) : ?>
-					<a class="more" href="<?php echo $item['link'] ?>">More</a>
-					<?php endif ?>
-				</div>
+			</a>
+			<?php endif ?>
 
-			</li>
-			<?php endforeach ?>
-		</ul>
+					<div class="content">
+						<?php echo $item['title'] ?>
+
+						<?php if (isset($item['name'])) : ?>
+						<?php if (isset($item['bioUrl'])) : ?>
+						<a href="<?php echo $item['bioUrl'] ?>">
+					<? endif ?>
+
+						<?php echo $item['name'] ?>
+
+						<?php if (isset($item['bioUrl'])) : ?>
+					</a>
+					<? endif ?>
+						<?php endif ?>
+
+						<?php if (isset($item['date'])) : ?>
+						<p class="date"><?php echo $item['date'] ?></p>
+						<?php endif ?>
+
+						<?php if (isset($item['link'])) : ?>
+						<a class="more" href="<?php echo $item['link'] ?>">More</a>
+						<?php endif ?>
+					</div>
+
+				</li>
+				<?php endforeach ?>
+			</ul>
+		</div>
 	</div>
 </div>
 <?php endif ?>
